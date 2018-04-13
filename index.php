eb55ebaad9cf5bec70e027c7ae34b3c4413e8713
Jamez <jamezblack19@gmail.com>
2018-04-13 04:19:27 +0100

    last commit

---
+++ index.php
@@ -0,0 +1,36 @@
+  </<!DOCTYPE html>
+  <html>
+  <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>HNG 4</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+      <script src="main.js"></script>
+  </head>
+  <body>
+  <div class="navcontainer">
+					<ul id="navbar">
+						<li><a href="#"> START </a> </li>
+						<li><a href="#"> ABOUT US </a></li>
+						<li><a href="#"> CONTACT US </a></li>
+					</ul>
+			</div> </br> </br> </br> </br> </br> </br>
+
+  <div class="text">
+    <p class="build">BUILD</p>
+    <p class="the">THE</p>
+    <p class="future">FUTURE</p>
+  </div> 
+
+  <div class="hng">
+    <p class="intern">HNG Internship 4!</p>
+  </div> 
+  
+  <p class="date">
+  <?php
+  echo  date("Y/m/d")  ." ".  date("h:i:sa");;
+  ?>
+  </p>
+  </body>
+  </html>
